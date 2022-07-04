d3a5ae2a87c20a2bad1b3ea4a53f2b4898c02538
NjengaErnest <gichuhiernest65@gmail.com>
2022-07-04 14:51:14 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - main
+ * print both lower and upper case
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c)
+c++;
+}
+putchar('\n');
+retun(0);
+}
